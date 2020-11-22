c866ff55918f326087c06f26e3a82b92bf582bc4
abhishek_on_linux <abhishek_on_linux@example.com>
2020-11-22 03:15:33 -0500

    able to load image. Article only listens to one event at the start. idk why.

--- saliency/src/components/ImageLoader.vue
+++ saliency/src/components/ImageLoader.vue
@@ -1,38 +1,55 @@
 <template>
-  <div>
-    <h1>Testing</h1>
-    <input type="file" @change="onFileSelected">
-    <img ref="input_image">
-    <!-- <WebCam/> -->
+  <div id="app">
+    <h2>Select an image</h2>
+    <img :src="image" />
+    <input type="file" v-on:change="onFileChange" />
   </div>
 </template>
+
 <script>
 // import { WebCam } from 'vue-web-cam';
 
 export default {
   name: 'ImageLoader',
-  data: function data() {
+  data() {
     return {
-      fileSelected: null,
+      image: '',
     };
   },
   components: {
     // WebCam,
   },
   methods: {
-    onFileSelected(event) {
-      this.fileSelected = event.target.files[0];
-      console.log(this.fileSelected);
+    onFileChange(e) {
+      const files = e.target.files || e.dataTransfer.files;
+      if (!files.length) return;
+      this.createImage(files[0]);
+      console.log('changed');
+      this.$emit('image-loaded', 1);
     },
-    // https://github.com/ashwinvaswani/tfjs-image-classification/blob/master/js/mobile-net.js
-    // async renderImage() {
-    //   var reader = new FileReader()
+    createImage(file) {
+      const reader = new FileReader();
+      const vm = this;
 
-    // },
+      reader.onload = (e) => {
+        vm.image = e.target.result;
+      };
+      reader.readAsDataURL(file);
+    },
   },
 };
 </script>
 
 <style>
-
+#app {
+  text-align: center;
+}
+img {
+  width: 30%;
+  margin: auto;
+  display: block;
+  margin-bottom: 10px;
+}
+button {
+}
 </style>
--- saliency/src/views/Article.vue
+++ saliency/src/views/Article.vue
@@ -2,60 +2,17 @@
   <div class="a">
     <ImageLoader/>
     <SaliencyMap/>
+    <div v-on:image-loaded="predictModel">prediction:</div>
 
     <h1>Visualizing Age Detectors with Saliency Maps</h1>
 
     <h2>What is a saliency map?</h2>
 
-    <p>
-      Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-      when an unknown printer took a galley of type and scrambled it to make a type
-      specimen book. It has survived not only five centuries, but also the leap
-      into electronic typesetting, remaining essentially unchanged.
-      It was popularised in the 1960s with the release of Letraset
-      sheets containing Lorem Ipsum passages, and more recently with
-      desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-      </p>
-
     <h2>How to use a saliency map</h2>
 
-    <p>
-      Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-      when an unknown printer took a galley of type and scrambled it to make a type
-      specimen book. It has survived not only five centuries, but also the leap
-      into electronic typesetting, remaining essentially unchanged.
-      It was popularised in the 1960s with the release of Letraset
-      sheets containing Lorem Ipsum passages, and more recently with
-      desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-      </p>
-
     <h2>What is a saliency map?</h2>
 
-    <p>
-      Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-      when an unknown printer took a galley of type and scrambled it to make a type
-      specimen book. It has survived not only five centuries, but also the leap
-      into electronic typesetting, remaining essentially unchanged.
-      It was popularised in the 1960s with the release of Letraset
-      sheets containing Lorem Ipsum passages, and more recently with
-      desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-      </p>
-
     <h2>What is a saliency map?</h2>
-
-    <p>
-      Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-      when an unknown printer took a galley of type and scrambled it to make a type
-      specimen book. It has survived not only five centuries, but also the leap
-      into electronic typesetting, remaining essentially unchanged.
-      It was popularised in the 1960s with the release of Letraset
-      sheets containing Lorem Ipsum passages, and more recently with
-      desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-      </p>
   </div>
 </template>
 
@@ -70,6 +27,7 @@ export default {
   data: function data() {
     return {
       model: '',
+      num: 1,
     };
   },
   props: {
@@ -83,9 +41,15 @@ export default {
       const modelFile = '../../model/model.json';
       this.model = await tf.loadLayersModel(modelFile);
     },
+
+    async predictModel(pic) {
+      this.num += pic;
+      console.log(this.num);
+    },
   },
   async mounted() {
     this.loadModel();
+    this.predictModel();
   },
 };
 </script>
