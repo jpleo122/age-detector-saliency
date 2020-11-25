2ca57b33faabef1b893cc296793276444155ceb5
Jonathan Leo <jpleo122@gmail.com>
2020-11-24 21:25:25 -0500

    Computed the pixel grandients based off the input image.

--- saliency/src/views/Article.vue
+++ saliency/src/views/Article.vue
@@ -6,7 +6,7 @@
     <SaliencyMap/>
     <!-- <div>prediction:{{this.num}}</div> -->
 
-    <img :src='this.temp_img_url' id='temp_image'>
+    <canvas id="myCanvas" width="200" height="100"></canvas>
 
     <h1>Visualizing Age Detectors with Saliency Maps</h1>
 
@@ -22,7 +22,8 @@
 
 <script>
 import * as tf from '@tensorflow/tfjs';
-import * as cv from 'opencv.js';
+// import * as axios from 'axios';
+// import * as cv from 'opencv.js';
 
 import SaliencyMap from '../components/SaliencyMap.vue';
 import ImageLoader from '../components/ImageLoader.vue';
@@ -51,11 +52,27 @@ export default {
       const modelFile = '../../models/model.json';
       this.model = await tf.loadLayersModel(modelFile);
     },
-    loadFaceDetection() {
-      const faceDetectFile = '../../haarcascade_frontalface_default.xml';
-      this.face_detector = new cv.CascadeClassifier();
-      this.face_detector.load(faceDetectFile);
-    },
+    // loadFaceDetection() {
+    //   // const faceDetectFile = '../../models/haarcascade_frontalface_default.xml';
+    //   this.face_detector = new cv.CascadeClassifier();
+    //   // this.face_detector.load(faceDetectFile);
+    //   // cv.FS_createLazyFile('/', 'haarcascade_frontalface_default.xml',
+    //   // 'haarcascade_frontalface_default.xml', true, false);
+    //   // this.face_detector.load('./tests/haarcascade_frontalface_default.xml');
+    //   // console.log('loaded');
+    //   // const img = new Image();
+    //   // const url = '../../greyscale.jpg';
+    //   // img.src = url;
+    //   // img.onload = () => {
+    //   //   const cvImg = cv.imread(img);
+    //   //   const imgGray = new cv.Mat();
+    //   //   cv.cvtColor(cvImg, imgGray, cv.COLOR_BGR2GRAY, 0);
+
+    //   //   const faces = new cv.RectVector();
+    //   //   this.face_detector.detectMultiScale(imgGray, faces, 1.2, 6, (100, 100));
+    //   //   console.log(faces);
+    //   // };
+    // },
     async predictModel(event) {
       this.pic = event;
       // console.log(this.pic);
@@ -66,6 +83,7 @@ export default {
       img.src = url;
       img.onload = () => {
         this.temp_tf_image = tf.browser.fromPixels(img, 1);
+        console.log(this.temp_tf_image);
         this.getPredictionAndGradients();
       };
     },
@@ -75,15 +93,32 @@ export default {
       tf.max(this.temp_tf_image).print();
       const outputs = this.model.predict(this.temp_tf_image.reshape([1, 200, 200, 1]));
       outputs.print();
-      this.predicted_idx = tf.argMax(outputs);
-      this.predicted_idx.print();
-      // const softmaxed = tf.softmax(outputs);
-      // console.log(outputs.dataSync()[0]);
+      this.predicted_idx = tf.argMax(outputs, 1).dataSync()[0];
+      // console.log(this.predicted_idx[0]);
+      const softmaxed = tf.softmax(outputs).dataSync();
+      console.log(softmaxed);
+      tf.tensor(softmaxed[0]).print();
+
       // obtain gradients
+      const predictFunc = (x) => {
+        return tf.softmax(this.model.predict(x.reshape([1, 200, 200, 1])));
+        // const tempPredicted = tf.argMax(tempOutputs, 1).dataSync()[0];
+        // return tf.softmax(;
+      };
+      const gradFunc = tf.grad(predictFunc);
+      // const grad = gradFunc(this.temp_tf_image, tf.tensor(tf.softmax(outputs).dataSync()[0]));
+      const grads = gradFunc(this.temp_tf_image, tf.softmax(outputs)).abs();
+      // console.log(255 / tf.max(grads).dataSync()[0]);
+      const newImage = grads.mul(1 / tf.max(grads).dataSync()[0]);
+      console.log(newImage);
+      tf.max(newImage).print();
+
+      const canvas = document.getElementById('myCanvas');
+      tf.browser.toPixels(newImage, canvas);
     },
   },
   async mounted() {
-    this.loadFaceDetection();
+    // this.loadFaceDetection();
     await this.loadModel();
     await this.getTFImage('../../greyscale.jpg');
   },
